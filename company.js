a73e0e771d37edba1bb555685653c234dfb5a67d
ikyase emmanuella nguavese <ikyase2@gmail.com>
2024-08-10 21:47:28 +0100

    first commit

---
+++ company.js
@@ -0,0 +1,155 @@
+
+
+let company = [
+  {
+    name: "snow business center",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business 2",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business 3",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business 4",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business5",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business6",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business7",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business8",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business9 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow business10 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise1 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise2",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise3 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise4 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise5 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise 6",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise 7",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise 8",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise9 ",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise30",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+  {
+    name: "snow enterprise30",
+    startDate: "12 / 8 / 1990",
+    endDate: "14 / 3 / 2021",
+  },
+];
+console.log(company);
